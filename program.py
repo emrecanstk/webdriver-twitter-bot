33c7d468a9d510992356cc81d3ca349d0ee68e90
emrecanstk <emrecanstk@gmail.com>
2022-07-31 21:32:24 +0300

    gundem konularina erisildi ve ekrana yazdirildi

--- program.py
+++ program.py
@@ -19,20 +19,30 @@ time.sleep(1)
 eposta_giris.send_keys(Keys.ENTER)
 
 try:
-    sifre_giris = driver.find_element(By.NAME,'password')
+    sifre_giris = tarayici.find_element(By.NAME,'password')
 except:
     time.sleep(2)
-    kullanici_adi_giris = driver.find_element(By.NAME,'text')
+    kullanici_adi_giris = tarayici.find_element(By.NAME,'text')
     kullanici_adi_giris.send_keys(kullanici_adi)
     kullanici_adi_giris.send_keys(Keys.ENTER)
 
 time.sleep(3)
-sifre_giris = driver.find_element(By.NAME,'password')
+sifre_giris = tarayici.find_element(By.NAME,'password')
 sifre_giris.click()
 sifre_giris.send_keys(sifre)
 sifre_giris.send_keys(Keys.ENTER)
 
 time.sleep(7)
+
+liste = tarayici.find_elements(By.CSS_SELECTOR,'.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0')
+
+for i in liste:
+    if '#' in i.text:
+        print(i.text)
+
+
+
+
 
 
 
