c331ea1c7eece59be7354a0e1e0cf50bc3ceed8a
emrecanstk <emrecanstk@gmail.com>
2022-07-31 19:52:54 +0300

    siteye yonlendirme guncellendi

--- program.py
+++ program.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 tarayici = webdriver.Chrome()
 
-tarayici.get("https://www.twitter.com")
+tarayici.get('https://twitter.com/i/flow/login')
+time.sleep(7)
+
+
+
