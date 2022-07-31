a9a1484b02f1a35fbdf393725f3a086be0e5f13f
emrecanstk <emrecanstk@gmail.com>
2022-07-31 20:32:14 +0300

    try except ile hata kontrolu yaptik

--- program.py
+++ program.py
@@ -18,6 +18,24 @@ eposta_giris.send_keys(eposta)
 time.sleep(1)
 eposta_giris.send_keys(Keys.ENTER)
 
+try:
+    sifre_giris = driver.find_element(By.NAME,'password')
+except:
+    time.sleep(2)
+    kullanici_adi_giris = driver.find_element(By.NAME,'text')
+    kullanici_adi_giris.send_keys(kullanici_adi)
+    kullanici_adi_giris.send_keys(Keys.ENTER)
+
+time.sleep(3)
+sifre_giris = driver.find_element(By.NAME,'password')
+sifre_giris.click()
+sifre_giris.send_keys(sifre)
+sifre_giris.send_keys(Keys.ENTER)
+
+time.sleep(7)
+
+
+
 
 
 
